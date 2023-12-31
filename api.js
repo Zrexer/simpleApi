5a15b6084f828871a1ce01e4a6e7df6037117327
Zrexer <122536817+Zrexer@users.noreply.github.com>
2023-12-31 21:53:13 +0330

    Update api.js

--- api.js
+++ api.js
@@ -1,24 +1,101 @@
 #!/usr/bin/env node 
 
-const alex = require('alexcolor/alexcolor/index');
+//----[ Modules ]----
+const alex = require('./node_modules/alexcolor/alexcolor/index');
 const express = require('express')();
-const dns = require("dns");
+const fs = require('fs');
+const request = require("./node_modules/request/index");
+const time = new Date();
 const https = require("https");
-const http = require('http');
+const http = require("http");
 const { exec } = require("child_process");
 
-const port = 4646;
-var something = 0;
+//----[ Variables ]----
 let somewhere = [];
+let howMuchConnection = 0;
 
+//----[ Data ]----
+const host = "127.0.0.1";
+const port = 4646;
+
+//----[ Started Data ]----
+const data = {
+    "hostName" : host,
+    "port" : port,
+    "starterTime" : time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()
+};
+
+//----[ Data Controller ]----
+class ControlData{
+    constructor(){
+        this.base = {
+            'Dev' : "Host1let"
+        };
+    }
+
+    addData(key, value){
+        try{
+            this.base[key] = value;
+            return JSON.parse(JSON.stringify({"status" : true, "main" : this.base}));
+        }catch (error){
+            return JSON.parse(JSON.stringify({"status" : false, "main" : error}));
+        }
+    }
+
+    deleteData(key){
+        try{
+            delete this.base[key];
+            return JSON.parse(JSON.stringify({"status" : true, "main" : this.base}));
+        }catch (error){
+            return JSON.parse(JSON.stringify({"status" : false, "main" : error}));
+        }
+    }
+
+    changeData(key, value){
+        try{
+            this.base[key] = value;
+            return JSON.parse(JSON.stringify({"status" : true, "main" : this.base}));
+        }catch (error){
+            return JSON.parse(JSON.stringify({"status" : false, "main" : error}));
+        }
+    }
+
+    allKeys(){
+        return JSON.parse(JSON.stringify({"status" : true, "main" : Object.keys(this.base)}));
+    }
+
+    allValues(){
+        return JSON.parse(JSON.stringify({"status" : true, "main" : Object.values(this.base)}));
+    }
+
+    clearAllData(){
+        let so = [];
+
+        for (let k in this.base){
+            so.push(k);
+        }
+
+        for (let i = 1; i < so.length; i++){
+            delete this.base[so[i]]
+        }
+
+        return JSON.parse(JSON.stringify({"status" : true, "main" : this.base}));
+    }
+}
+
+//----[ Call Class ]----
+const controller = new ControlData();
+
+//----[ Create Random INT Data]----
 function rc(){
     exec("python handlerint.py", (err, stdout, stderr) => {
     somewhere.push(stdout);
-    console.log(somewhere[somewhere.length - 1]);
+    //console.log(somewhere[somewhere.length - 1]);
     return somewhere[somewhere.length - 1];
     });
 }
 
+//----[ Create a Random String Data ]----
 function generateRandomString(len) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -31,178 +108,19 @@ function generateRandomString(len) {
     return result;
 };
 
-class BaleMessenger{
-    constructor(token){
-        this.token = String(token);
-        this.baseUrl = `https://tapi.bale.ai/bot${this.token}/`
-    }
-/*
-    post(method){
-        try{
-            https.get({
-                hostname: "tapi.bale.ai",
-                port: 443,
-                path: `/bot${this.token}/${method}`,
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer your_token'
-                  }
-            }, (dataForPost) => {
-                dataForPost.on('data', (chForPost) => {
-                    return JSON.parse(chForPost);
-                })
-            })
-        }catch (error){
-            return error;
-        }
-    }
-
-    get(method){
-        try{
-            https.get({
-                hostname: "tapi.bale.ai",
-                port: 443,
-                path: `/bot${this.token}/${method}`,
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer your_token'
-                  }
-            }, (dataForGet) => {
-                dataForGet.on('data', (chForGET) => {
-                    return chForGET;
-                })
-            })
-        }catch (error){
-            return error;
-        }
-    }
-*/
-    sendMessage(text = null, chat_id = null, reply_to_message_id = null){
-        if (text === null || chat_id === null){
-            return "The [ text , chat_id ]parameter cannot be empty";
-        }else{
-            try{
-                https.get({
-                    hostname: "tapi.bale.ai",
-                    port: 443,
-                    path: `/bot${this.token}/sendMessage?chat_id=${chat_id}&text=${text}&reply_to_message_id=${reply_to_message_id}`,
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer your_token'
-                      }
-                }, (dataForPost) => {
-                    dataForPost.on('data', (chForPost) => {
-                        return chForPost;
-                    })
-                })
-            }catch (error){
-                return error;
-            }
-        }
-    }
-}
-
-module.exports()
-
-const x = BaleMessenger("777813486:emucVlckFMmyHXAVMxnnPTq5Wy66inELvfPoiL5T");
-
-console.log(x.sendMessage("Hello WOrld", "554324725"))
-
-express.listen(port, () => {
-    console.log(`Server (${alex.red('127.0.0.1')}) Running on ${alex.yellow(port)}`);
+//----[ Go On Listen Mode ]----
+express.listen(port, host, () => {
+    console.log(alex.blue("Running ON ") + alex.red(host+alex.yellow(":")+alex.red(port)));
+    console.log(JSON.parse(JSON.stringify(data)));
+    console.log("\n");
 })
 
-express.get("/randomint", (req, res, nx) => {
-    something += 1;
-    console.log(`The ${alex.red(something)} is Connected To Server`);
-    console.log(`Request for ${alex.green("/randomint")}\n`);
-
-    var op = Math.random();
-
-    res.send(
-        {
-            "Dev" : "Host1let",
-            "data" : String(op)
-        }
-    );
-})
-
-express.get("/randomstr", (req, res, nx) => {
-    something += 1;
-    console.log(`The ${alex.red(something)} is Connected To Server`);
-    console.log(`Request for ${alex.green("/randomstr")}\n`);
-
-    const much = req.query.much;
-
-    rc();
-
-    if (much === undefined){
-        res.send(
-            {
-                'Dev' : "Host1let",
-                "data" : generateRandomString(parseInt(String(somewhere[somewhere.length - 1]).replace("\n", "").replace("\r", ''))),
-                "dataType" : "string",
-                "dataLength" : String(somewhere[somewhere.length - 1]).replace("\n", "").replace("\r", '')
-            }
-        );
-    }else{
-        res.send(
-            {
-                'Dev' : "Host1let",
-                "data" : generateRandomString(parseInt(much)),
-                "dataType" : "string",
-                "dataLength" : much
-            }
-        );
-    }
-})
-
-express.get("/lookup", (req, res, nx) => {
-    something += 1;
-    console.log(`The ${alex.red(something)} is Connected To Server`);
-    console.log(`Request for ${alex.green("/lookup")}\n`);
-
-    const domainForScan = req.query.domain;
-
-    if (domainForScan === undefined){
-        res.send(
-            {
-                'Dev' : "Host1let",
-                "data" : "Please Set the Host: /lookup?domain=DOMAIN"
-            }
-        )
-    }else{
-        dns.lookup(domainForScan, (err, addr) => {
-        if (err){
-            res.send(
-                {
-                    'Dev' : "Host1let",
-                    "data" : err
-                }
-            )
-        }else{
-            res.send(
-                {
-                    'Dev' : "Host1let",
-                    "data" : addr
-                    }
-                )
-            }
-        })
-
-    }
-})
-
-express.get("/gpt4", (req, res, nx) => {
-    something += 1;
-    console.log(`The ${alex.red(something)} is Connected To Server`);
-    console.log(`Request for ${alex.green("/gpt4")}\n`);
-    
+//----[ Chat GPT ]----
+express.get("/gpt", (req, res, nx) => {
+    howMuchConnection += 1;
+    console.log(alex.yellow("{ ") + alex.green(`${howMuchConnection} `) + alex.yellow("} ") + alex.blue("Requested For ") + alex.yellow("[ ") + alex.red("/gpt")+  alex.yellow(" ]"));
+   
     const text = req.query.text;
-
     if (text === undefined){
         res.send(
             {
@@ -220,6 +138,58 @@ express.get("/gpt4", (req, res, nx) => {
                         'data' : JSON.parse(ch).result.answer
                     }
                 );
+            })
+        })
+    }
+})
+
+//----[ Random String ]----
+express.get("/random/str", (req, res, nx) => {
+    howMuchConnection += 1;
+    console.log(alex.yellow("{ ") + alex.green(`${howMuchConnection} `) + alex.yellow("} ") + alex.blue("Requested For ") + alex.yellow("[ ") + alex.red("/random/str")+  alex.yellow(" ]"));
+ 
+    const much = req.query.much;
+    rc();
+
+    if (much === undefined){
+        controller.clearAllData();
+        controller.addData("data", generateRandomString(parseInt(String(somewhere[somewhere.length - 1]).replace("\n", "").replace("\r", ''))));
+        controller.addData('dataType', "string");
+        res.send(controller.addData('dataLength', String(somewhere[somewhere.length - 1]).replace("\n", "").replace("\r", '')).main);
+
+    }else{
+        controller.clearAllData();
+        controller.addData("data", generateRandomString(parseInt(much)));
+        controller.addData("dataType", "string");
+        res.send(controller.addData('dataLength', much).main);
+    }
+})
+
+//----[ Wikipedia Searcher ]----
+express.get("/wiki", (req, res, nx) => {
+    howMuchConnection += 1;
+    console.log(alex.yellow("{ ") + alex.green(`${howMuchConnection} `) + alex.yellow("} ") + alex.blue("Requested For ") + alex.yellow("[ ") + alex.red("/wiki")+  alex.yellow(" ]"));
+ 
+    const q = req.query.query;
+    if (q === undefined) {
+       controller.clearAllData();
+       controller.addData("ThanksTo", "CodeBazan");
+       controller.addData("data", "Error; Please Set the query Like: /wiki?query=Iran");
+       res.send(controller.addData('dataType', "string").main);
+
+    }else{
+        http.get(`http://api.codebazan.ir/wiki/?search=${q}`, (resx) => {
+            let d = '';
+
+            resx.on("data", (ch) => {
+                d += ch;
+            })
+
+            resx.on("end", () => {
+                controller.clearAllData();
+                controller.addData("ThanksTo", "CodeBazan");
+                controller.addData("data", d);
+                res.send(controller.addData("dataType", "string").main)
             })
         })
     }
